8b98bbcb7f7d9b42b43f3fbc3267c50c0b1bd936
David <davitorgar03@gmail.com>
2026-06-19 03:08:59 +0200

    Añadiendo requirements

--- app.py
+++ app.py
@@ -344,5 +344,6 @@ def offline():
 # Run
 # ----------------------
 if __name__ == '__main__':
-    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
